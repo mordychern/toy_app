f1f19008552477c41dea4dd0f462b32e6938e940
Mordy Chern <mordychern94@gmail.com>
2016-02-10 18:24:47 +0000

    Micropost Rollback

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,4 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   validates :content, length: { maximum: 140 }, 
                       presence: true
-  validates FILL_IN, presence: true
-  validates FILL_IN, presence: true             
 end
